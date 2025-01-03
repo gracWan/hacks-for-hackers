55cb83e0dc3bfcfabf66234695c4950a060579cb
Grace Wang <gwwang0925@gmail.com>
2025-01-03 13:16:11 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from './components/Login'
 import Register from "./components/Register"
+import Navbar from './components/Navbar'
 
 export default function App() {
-
   return (
-    <>
-      <Login/>
-      <Register/>
-    </>
+    <Navbar/>
+    /*<BrowserRouter> 
+      <Routes>
+        <Route path = "/hacks-for-hackers" element ={<Login/>}/>
+      </Routes>
+    </BrowserRouter>*/
   )
 }
