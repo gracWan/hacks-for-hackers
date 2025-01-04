5e6a804ced48cd674f6352b6467f099cb46cc2ee
NataliaDzielnicka <68565479+NataliaDzielnicka@users.noreply.github.com>
2025-01-04 17:44:14 -0500

    Create SideNav.jsx

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,10 @@
+export default function SideNav() {
+    return (
+        <>
+            <div className="sidenav">
+                <a href="#about" style={{ writingMode: "sideways-lr" }}>Learn</a>
+                <a href="#services" style={{ writingMode: "sideways-lr" }}>Teach</a>
+            </div>
+        </>
+    );
+}
