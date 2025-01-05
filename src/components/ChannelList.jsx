5b28f5c139a58b0c9d7edeb5d650ca71188429b9
gracWan <gwwang0925@gmail.com>
2025-01-04 19:44:41 -0500

    Create ChannelList.jsx

---
+++ src/components/ChannelList.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  useCreateChatClient,
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from 'stream-chat-react';
+
+import 'stream-chat-react/dist/css/v2/index.css';
+import './layout.css';
+
+const apiKey = 'cyaw4h7bya7e';
+const userId = 'grace';
+const userName = 'grace';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ3JhY2UifQ.iST__vuUjbnraFvRoGjV1DncZKmXNsoEkPVc191Pw2w';
+
+const user = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+
+const sort = { last_message_at: -1 };
+const filters = {
+  type: 'messaging',
+  members: { $in: [userId] },
+};
+const options = {
+  limit: 10,
+};
+
+const Message = () => {
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
+  return (
+    <Chat client={client}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default Message;
